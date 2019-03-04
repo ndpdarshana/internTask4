b4db34c733716ebe76fd4ccf8fdc7c7e2d1e405f
ndpdarshana <ndp.mymail@gmail.com>
2019-03-04 23:59:42 +1100

    optional development - Not completed

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var app = angular.module('task4',[]);
+var app = angular.module('task4',['vcRecaptcha']);
 app.controller('mainController', function($scope){
   $scope.data = localStorage.getItem('msg');
+
+  $scope.setResponse = function (response) {
+    
+  };
+
+  $scope.cbExpiration = function() {
+
+  };
 
   $scope.send = function(text){
     var expr = /query/;
@@ -9,6 +17,7 @@ app.controller('mainController', function($scope){
       $scope.data = 'You can not enter messages containing word' + expr;
     }else{
       $scope.date = new Date();
+      console.log(recaptcha);
       $scope.data = text + " " + $scope.date;
       localStorage.setItem('msg', $scope.data);
     }
